830fed0b333e6927fad28601101b74134f6aee3a
Sergio2R <searodriguezr@correo.udistrital.edu.co>
2022-06-06 23:00:24 -0500

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BeerService } from './api/services';
+import { Beer } from './api/models';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'appATTAngularFront';
+  public beer: Beer = {};
+  public constructor(private api: BeerService){
+    this.api.apiBeerGet$Json({Id : 1}).subscribe(rta => {
+      this.beer = rta;
+    });
+  }
 }
