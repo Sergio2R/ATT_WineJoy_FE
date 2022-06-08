30a5881fefa7f25f126ed71a2c24a7a377001cb6
Sergio2R <searodriguezr@correo.udistrital.edu.co>
2022-06-08 09:31:41 -0500

    .

--- src/app/wine/services/clasification.service.ts
+++ src/app/wine/services/clasification.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Clasification } from '../interfaces/clasification.interface';
-import {Router, NavigationEnd,ActivatedRoute} from '@angular/router';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 
 import { ClasificationService as ApiClasificationService } from 'src/app/api/services';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
@@ -22,7 +22,7 @@ export class ClasificationService {
   }
 
   getClasification(id: number) {
-    this.api.apiClasificationGetClasificationGet$Json( { Id: id }).subscribe(rta => {
+    this.api.apiClasificationGetClasificationGet$Json({ Id: id }).subscribe(rta => {
       return rta;
     });
   }
